fee83d8483b50f35019635140277d0d29f6361e7
Dandi Indra Wijaya <dandiindra29@gmail.com>
2020-10-29 14:48:34 +0700

    Make handler for failed routing

--- service-worker.js
+++ service-worker.js
@@ -1,67 +1,69 @@
-let CACHE_NAME = "top-euro-league-v1";
-let urlsToCache = [
-  "/",
-  "/nav.html",
-  "/match.html",
-  "/club_information.html",
-  "/standings.html",
-  "/index.html",
-  "/pages/home.html",
-  "/pages/about.html",
-  "/pages/my_favorite_clubs.html",
-  "/css/materialize.min.css",
-  "/css/style.css",
-  "/images/logo.png",
-  "/images/bundesliga.svg",
-  "/images/eredivisie.jpg",
-  "/images/la_liga.png",
-  "/images/ligue_1.png",
-  "/images/premiere_league_emblem.jpg",
-  "/images/serie_a.jpg",
-  "/js/materialize.min.js",
-  "/manifest.json",
-  "icons/iphone/apple-launch-1125x2436.png",
-  "icons/icon-128x128.png",
-  "icons/icon-144x144.png",
-  "icons/icon-192x192.png",
-  "icons/icon-256x256.png",
-  "icons/icon-384x384.png",
-  "icons/icon-512x512.png",
-  "/js/db.js",
-  "/js/idb.js",
-  "/js/main.js",
-  "/js/api.js",
-];
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+ 
+if (workbox)
+  console.log(`Workbox berhasil dimuat`);
+else
+  console.log(`Workbox gagal dimuat`);
 
 
-self.addEventListener("fetch", function(event) {
-  let base_url = "https://api.football-data.org/v2/";
+workbox.precaching.precacheAndRoute([
+    { url: '/', revision: '1'},
+    { url: '/manifest.json', revision: '1' },
+    { url: '/offline_page.html', revision: '1' },
+    { url: '/index.html', revision: '1' },
+    { url: '/nav.html', revision: '1' },
+    { url: '/match.heml', revision: '1' },
+    { url: '/club_information.html', revision: '1' },
+    { url: '/standing.html', revision: '1' },
+    { url: '/pages/home.html', revision: '1' },
+    { url: '/pages/about.html', revision: '1' },
+    { url: '/pages/my_favorite_clubs.html', revision: '1' },
+    { url: '/css/style.css', revision: '1' },
+    { url: '/css/materialize.min.css', revision: '1' },
+    { url: '/js/materialize.min.js', revision: '1' },
+    { url: '/js/main.js', revision: '1' },
+    { url: '/js/idb.js', revision: '1' },
+    { url: '/js/db.js', revision: '1' },
+    { url: '/js/api.js', revision: '1' },
+    { url: '/images/logo.png', revision: '1' },
+    { url: '/images/premiere_league_emblem.jpg', revision: '1' },
+    { url: '/images/bundesliga.svg', revision: '1' },
+    { url: '/images/la_liga.png', revision: '1' },
+    { url: '/images/ligue_1.png', revision: '1' },
+    { url: '/images/eredivisie.jpg', revision: '1' },
+    { url: '/images/serie_a.jpg', revision: '1' }, 
+    { url: 'icons/iphone/apple-launch-1125x2436.png', revision:'1' },
+    { url: 'icons/icon-128x128.png', revision: '1' },
+    { url: 'icons/icon-144x144.png', revision: '1' },
+    { url: 'icons/icon-192x192.png', revision: '1' },
+    { url: 'icons/icon-256x256.png', revision: '1' },
+    { url: 'icons/icon-384x384.png', revision: '1' },
+    { url: 'icons/icon-512x512.png', revision: '1' },
+]);
 
-  if (event.request.url.indexOf(base_url) > -1) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return fetch(event.request).then(function(response) {
-          cache.put(event.request.url, response.clone());
-          return response;
-        })
-      })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
-        return response || fetch (event.request);
-      })
-    )
+workbox.routing.registerRoute(
+  new RegExp('https://api.football-data.org/v2/'),
+  workbox.strategies.staleWhileRevalidate()
+);
+
+
+const offlinePage = '/offline/';
+/**
+ * Pages to cache
+ */
+workbox.routing.registerRoute(
+  new RegExp('/pages/'),
+  async ({event}) => {
+    try {
+      return await workbox.strategies.staleWhileRevalidate({
+          cacheName: 'cache-pages'
+      }).handle({event});
+    } catch (error) {
+      return caches.match(offlinePage);
+    }
   }
-});
+);
 
 self.addEventListener('push', function(event) {
   let body;
@@ -84,18 +86,3 @@ self.addEventListener('push', function(event) {
   );
 });
 
-
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName != CACHE_NAME) {
-            console.log("ServiceWorker: cache " + cacheName + " dihapus");
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
